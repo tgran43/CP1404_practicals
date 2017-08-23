83a248abe262f2dbbca1b939c3f164158676e8ed
Trey <tgran43@ymail.com>
2017-08-23 14:42:35 +1000

    reformat code

--- prac_04/5_numbers.py
+++ prac_04/5_numbers.py
@@ -6,4 +6,3 @@ print("The last number is ", numbers[-1])
 print("The smallest number is ", min(numbers))
 print("The largest number is ", max(numbers))
 print("The average of the numbers is", sum(numbers) / len(numbers))
-
--- prac_04/lotto_ticket_generator.py
+++ prac_04/lotto_ticket_generator.py
@@ -1,15 +1,15 @@
 import random
+
 quick_picks = []
 number_of_quick_picks = int(input("How many quick picks? "))
 for number in range(number_of_quick_picks):
     for i in range(6):
         quick_pick = random.randint(1, 46)
         while quick_pick in quick_picks:
-            quick_pick = random.randint(1,46)
+            quick_pick = random.randint(1, 46)
         quick_picks.append(quick_pick)
     quick_picks.sort()
     for i in quick_picks:
-        print("{:2}".format(i), end= " ")
+        print("{:2}".format(i), end=" ")
     print()
     quick_picks = []
-
