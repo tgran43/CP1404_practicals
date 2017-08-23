b3950a75f77562682a732f55f3a09e08766b5c77
Trey <tgran43@ymail.com>
2017-08-23 14:45:02 +1000

    reformat code

--- prac_04/cumlative_total_income.py
+++ prac_04/cumlative_total_income.py
@@ -13,13 +13,13 @@ def main():
         income = float(input("Enter income for month {}: ".format(month)))
         incomes.append(income)
 
-    report_printing(incomes, no_of_months)
+    report_printing(incomes)
 
 
-def report_printing(incomes, no_of_months):
+def report_printing(incomes):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, no_of_months + 1):
+    for month in range(1, len(incomes) + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
